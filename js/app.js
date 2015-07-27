5287275854517b17254cc6bdf1a736cfbf668680
root <root@ip-172-31-11-47.ec2.internal>
2015-07-27 23:30:28 +0000

    First commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("animationApp",['ngAnimate']).controller("mainController", function($scope){
+
+	$scope.fruits = ["Apple","Apricot","Avocado","Banana","Breadfruit","Bilberry","Blackberry","Blackcurrant","Blueberry","Boysenberry","Cantaloupe","Currant","Cherry","Cherimoya","Cloudberry","Coconut","Cranberry","Cucumber","Damson","Date","Dragonfruit","Durian"];
+
+	$scope.delete = function (index) {
+		var deleteFruit = $scope.fruits[index];
+
+		$scope.fruits.splice(index, 1);
+	};
+});
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+# angular-list-example
+A simple list (of fruits) developed with angularJS and Material Design Lite w/ add, remove and filter functionalities.
